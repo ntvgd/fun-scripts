e63893babab851b26902e5b4a96081ece342c7b2
ntvgd <nicholastrichardson@gmail.com>
2026-08-22 01:46:49 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,10 @@
-local Base = "https://raw.githubusercontent.com/ntvgd/fun-scripts/main/"
-local Files = {
+local Base="https://raw.githubusercontent.com/ntvgd/fun-scripts/main/"
+local Files={
     "my_fun_scripts_v2_gui_1.lua",
     "my_fun_scripts_v2_gui_2.lua",
 }
-local Source = {}
-for _, File in ipairs(Files) do
-    Source[#Source + 1] = game:HttpGet(Base .. File)
+local Source={}
+for _,File in ipairs(Files) do
+    Source[#Source+1]=game:HttpGet(Base..File)
 end
-loadstring(table.concat(Source, "\n"))()
+loadstring(table.concat(Source,"\n"))()
