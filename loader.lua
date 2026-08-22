f8154a268c5dafa3ca4136e80166326d9ebe4864
ntvgd <nicholastrichardson@gmail.com>
2026-08-22 01:49:27 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,8 +3,11 @@ local Files={
     "my_fun_scripts_v2_gui_1.lua",
     "my_fun_scripts_v2_gui_2.lua",
 }
+
 local Source={}
+
 for _,File in ipairs(Files) do
     Source[#Source+1]=game:HttpGet(Base..File)
 end
+
 loadstring(table.concat(Source,"\n"))()
